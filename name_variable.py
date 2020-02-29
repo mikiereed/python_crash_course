24925b8060f03f017bc4164b009cbfe5dce4b4fb
mikie Reed <mikie@prenotions.com>
2020-02-29 09:26:53 -0800

    add name cases to name_variable.py

--- name_variable.py
+++ name_variable.py
@@ -1,3 +1,9 @@
+# name message
 name = "mikie"
 message = f"Hello {name}, would you like to learn some Python today?"
 print(message)
+
+# name cases
+message = name.upper()
+print(message)
+print (message.title())
